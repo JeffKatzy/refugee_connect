66f5ef460bd609389c2afe1a3983d2de9bc6b233
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2014-01-20 15:33:33 -0500

    added groundwork css and styling to homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+//= require groundworkcss/libs/modernizr-2.6.2.min
+//= require groundworkcss/all
 //= require jquery_nested_form
+//= require groundwork/js/groundwork.all.js
 //= require 'connect.js'
