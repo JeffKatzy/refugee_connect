09f4d993a73ffed0f607e6665edaf251ede5c1fd
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2014-02-23 23:24:36 -0500

    fix bug in text_to_user

--- app/models/text_to_user.rb
+++ app/models/text_to_user.rb
@@ -22,8 +22,8 @@ class TextToUser < ActiveRecord::Base
   @@account = @@client.account
 
   def self.deliver(user, body)
-  	@message = @@account.sms.messages.create(:from => '+12673172085', :to => user.cell_number, :body => body, :status_callback => BASE_URL + "text_to_users/complete/#{self.id}.xml")
-  	TextToUser.create(body: body, user_id: user.id, time: Time.now.in_time_zone('Eastern Time (US & Canada)'))
+  	@text = TextToUser.create(body: body, user_id: user.id, time: Time.now.in_time_zone('Eastern Time (US & Canada)'))
+    @message = @@account.sms.messages.create(:from => '+12673172085', :to => user.cell_number, :body => body, :status_callback => BASE_URL + "text_to_users/complete/#{@text.id}.xml")
   	puts @message 
   end
 end
