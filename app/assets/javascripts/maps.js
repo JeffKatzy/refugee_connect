3a5a6866ac5cfc98818c4a596d26a3908c8e1eae
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2014-03-23 13:57:28 -0400

    change map to satellite view

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,7 +3,7 @@ function display_map(lat, long)
   var mapOptions = {
     center: new google.maps.LatLng(lat, long),
     zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.SATELLITE
   };
  
   canvas = $('#map_canvas')[0];
@@ -15,3 +15,4 @@ function add_marker(lat, long, title)
   var latlng = new google.maps.LatLng(lat, long);
   var marker = new google.maps.Marker({position: latlng, map: map, title: title});
 }
+
